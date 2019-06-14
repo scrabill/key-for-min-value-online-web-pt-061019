d03d6d3c054f538c3fcb0a35da27b8731000f2df
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-14 20:16:10 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,16 @@
 
 def key_for_min_value(name_hash)
 
+  low_key = nil
+  low_value = 999999999
+
+  # ikea = {:chair => 25, :table => 85, :mattress => 450}
+
+  name_hash.each do |key,value|
+    if value < low_value
+      low_value = value
+      low_key = key
+    end
+  end
+  low_key
 end
