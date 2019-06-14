bf87b43eb89c4bf846c137332d8364bba4d242f5
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-14 19:35:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,21 @@
 
 def key_for_min_value(name_hash)
 
+  # key all of the values, put them in a new array, sort, then return the correspoding ket
+
+  i = 0
+  while i < name_hash.length
+
+    name_hash.each do |item,key|
+      puts "#{item} + #{i}"
+
+      i + 1
+    end
+
+
+
+  end
+
+  # arr.sort{|x, y| x.length <=> y.length}
+
 end
